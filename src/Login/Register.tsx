d96b7147d4aee77a3d012471c3ddb787f01797df
Kiersten Grieco <kierstengrieco@kings.edu>
2024-03-24 12:07:59 -0400

    Raw login page, created registration page, and login reducer.

--- src/Login/Register.tsx
+++ src/Login/Register.tsx
@@ -5,18 +5,29 @@ import { accounts } from "../Database";
 function Login() {
     const [thisUser, setThisUser] = useState('');
     const [thisPassword, setThisPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [thisFirstName, setThisFirstName] = useState('');
+    const [thisLastName, setThisLastName] = useState('');
     
     return (
         <div>
             <hr />
-            Login
+            Register
             <span>
-                Don't have an account? Register <Link to={`/Login/Register`}>here</Link>
+                Have an account? Login <Link to={`/Login`}>here</Link>
             </span>
+            <div>
+                First Name
+                <input type="text" className="form-control" onChange={(e) => setThisFirstName(e.target.value)}/>
+                Last Name
+                <input type="text" className="form-control" onChange={(e) => setThisLastName(e.target.value)}/>
+            </div>
             Email
             <input type="text" className="form-control" onChange={(e) => setThisUser(e.target.value)} />
             Password
             <input type="text" className="form-control" onChange={(e) => setThisPassword(e.target.value)}/>
+            Re-Type Password
+            <input type="text" className="form-control" onChange={(e) => setPasswordCheck(e.target.value)}/>
             <hr />
             <div className="float-end">
                 <button className="btn">Cancel</button>
