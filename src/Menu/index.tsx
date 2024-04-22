017b462429a5c0f54508a23cacd24e928f205933
sarzig <63985693+sarzig@users.noreply.github.com>
2024-04-22 00:38:02 -0400

    improved hero image to span across entire page.

--- src/Menu/index.tsx
+++ src/Menu/index.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import CollapsibleSection from "../Admin/CollapsibleSection";
-import { Link } from "react-router-dom";
 import "./index.css";
 
 const Menu = () => {
@@ -27,11 +25,15 @@ const Menu = () => {
                 <h1>Menus</h1>
             </div>
 
-            <span style={{ maxWidth: "100%" }}>
-                <a href={`/images/menus/hero2.jpg`} target="_blank" rel="noopener noreferrer">
-                    <img src={`/images/menus/hero2.jpg`} alt={`/images/menus/hero2.jpg`} style={{ maxWidth: "100%" }} />
-                </a>
-            </span>
+            {/* Hero image visible only on large screens (lg and up) */}
+            <div className="d-none d-lg-block">
+                <div className="text-center">
+                    {/* Use a wrapper div to control width */}
+                    <div style={{ maxWidth: "100%" }}>
+                        <img src={`/images/menus/hero2.jpg`} alt="A lucious capuccino with a foam heart sits on a dark blue background with scattered candied fruit." style={{ width: "100%" }} />
+                    </div>
+                </div>
+            </div>
 
             <div className="container">
 
@@ -56,11 +58,15 @@ const Menu = () => {
                 </div>
             </div>
 
-            <span style={{ maxWidth: "100%" }}>
-                <a href={`/images/menus/hero.jpg`} target="_blank" rel="noopener noreferrer">
-                    <img src={`/images/menus/hero.jpg`} alt={`/images/menus/hero.jpg`} style={{ maxWidth: "100%" }} />
-                </a>
-            </span>
+            {/* Hero image visible only on large screens (lg and up) */}
+            <div className="d-none d-lg-block">
+                <div className="text-center">
+                    {/* Use a wrapper div to control width */}
+                    <div style={{ maxWidth: "100%" }}>
+                        <img src={`/images/menus/hero.jpg`} alt="A turkish coffee is to the right of this banner image. It is foamy with a heart on the top. Coffee beans and leaves are scattered on a rustic table in the background." style={{ width: "100%" }} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
