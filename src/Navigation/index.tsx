811ef3e7d28a65722ccdd6bf8953ef959c1a486d
sarzig <63985693+sarzig@users.noreply.github.com>
2024-04-22 21:29:21 -0400

    menu not done yet but at least hamburger looks good.

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -1,36 +1,73 @@
 // Navigation.js
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 import { FaBars } from 'react-icons/fa';
-import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MobilePopupMenu from "./MobilePopupMenu";
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Navigation({ userType }: any,) {
+const Navigation = ({ userType }: { userType: any }) => {
     const links = [
-        { label: "Menu", userTypes: ["admin", "customer", "owner", "guest"] },
-        { label: "Search", userTypes: ["admin", "customer", "owner", "guest"] },
-        { label: "Admin-Tools", userTypes: ["admin"] },
-
-        { label: "Profile", userTypes: ["admin", "customer", "owner"] },
-        { label: "All-Profiles", userTypes: ["admin", "customer", "owner", "guest"] },
-        { label: "Login-~-Signup", userTypes: ["guest"] },
-    ];
-
-    const linksXS = [
-        { label: "Menu", userTypes: ["admin", "customer", "owner", "guest"] },
-        { label: "drop-down", userTypes: ["admin", "customer", "owner", "guest"] },
-        { label: "Profile", userTypes: ["admin", "customer", "owner"] },
-        { label: "Login-~-Signup", userTypes: ["guest"] },
+        {
+            label: "Menu",
+            path: "Menu",
+            stringComprehension: "/Menu",
+            userTypes: ["admin", "customer", "owner", "guest"]
+        },
+        {
+            label: "Search Recipes",
+            path: "Search",
+            stringComprehension: "/Search",
+            userTypes: ["admin", "customer", "owner", "guest"]
+        },
+        {
+            label: "Admin-Tools",
+            path: "Admin-Tools",
+            stringComprehension: "/Menu",
+            userTypes: ["admin"]
+        },
+        {
+            label: "Profile",
+            path: "Profile",
+            stringComprehension: "Pro",
+            userTypes: ["admin", "customer", "owner"]
+        },
+        {
+            label: "All-Profiles",
+            path: "All-Profiles",
+            stringComprehension: "All-Profiles",
+            userTypes: ["admin", "customer", "owner", "guest"]
+        },
+        {
+            label: "Login-~-Signup",
+            path: "Login-~-Signup",
+            stringComprehension: "Login",
+            userTypes: ["guest"]
+        },
     ];
 
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     const toggleDropdown = () => {
-        //setIsDropdownOpen(!isDropdownOpen);
+        setIsDropdownOpen(!isDropdownOpen);
     };
 
     const { pathname } = useLocation();
+    console.log(pathname);
+
+    const makeMenuUL = () => (
+        <ul className="menu-list">
+            {links.map((link, index) => (
+                link.userTypes.includes(userType) && (
+                    <li key={index} className={pathname.includes(`/${link.path}`) ? "menu-active" : ""}>
+                        <Link to={`/${link.path}`}>{link.label.replace(/-/g, ' ')}</Link>
+                    </li>
+                )
+            ))}
+        </ul>
+    );
 
     return (
         <div className="row fixed-top-row">
@@ -39,40 +76,24 @@ export default function Navigation({ userType }: any,) {
             </h2>
             <div className="row custom-nav-row g-0">
                 <div className="col d-none d-sm-block">
-                    <ul className="menu-list">
-                        {links.map((link, index) => (
-                            link.userTypes.includes(userType) && (
-                                <li key={index} className={pathname.includes(link.label) ? "menu-active" : ""}>
-                                    <Link to={`/${link.label}`}>{link.label.replace(/-/g, ' ')}</Link>
-                                </li>
-                            )
-                        ))}
-                    </ul>
+                    {makeMenuUL()}
                 </div>
 
                 <div className="col d-block d-sm-none">
                     <ul className="menu-list">
-                        {linksXS
-                            .filter(link => link.userTypes.includes(userType))
-                            .map((link, index) => (
-                                <li key={index} className={pathname.includes(link.label) ? "wd-active" : ""}>
-                                    {link.label === "drop-down" ?
-                                        (
-                                            <Link to={`/${link.label}`} onClick={toggleDropdown}>
-                                                <FaBars />
-                                            </Link>
-                                        ) : (
-                                            <Link to={`/${link.label}`}>{link.label}</Link>
-                                        )
-                                    }
-                                </li>
-                            ))}
+                        <li className="icon-container" onClick={toggleDropdown}>
+                            <FaBars className="big-icon icon"/>
+                        </li>
                     </ul>
-                    <div>
-                        {isDropdownOpen && <MobilePopupMenu />}
-                    </div>
+                    {isDropdownOpen && (
+                        <div>
+ahhh
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
     );
 };
+
+export default Navigation;
